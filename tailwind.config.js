717899bdc467f02d43bb3fccec2d684a2049318b
ShinobiSaad <saad.syedkabir@gmail.com>
2023-08-22 23:17:23 +0600

    Added Image static files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./potential/templates/frontpage/*.html"],
+  content: ["./*/templates/*/*.html", "./*/templates/*/*/*.html", "./*/*.py"],
   theme: {
     extend: {},
   },
